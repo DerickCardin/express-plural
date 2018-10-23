ffbc294996a4e7f6bac12c8a111a6a642d1e8272
Derick Cardin <derickc50@gmail.com>
2018-10-23 01:12:52 +0000

    Finished user params

--- app.js
+++ app.js
@@ -13,13 +13,45 @@ var blocks ={
     'Rotating': 'Moving in a cirlce around its center'
 };
 
+var locations = {
+    'Fixed': 'First floor',
+    'Movable': 'Second floor',
+    'Rotating': 'Penthouse'
+};
+
+app.param('name', function(request, response, next) {
+    var name = request.params.name;
+    var block = name[0].toUpperCase() + name.slice(1).toLocaleLowerCase();
+    
+    request.blockName = block;
+    
+    next();
+});
+
 app.get('/blocks/:name', function(request, response) {
-    var description = blocks[request.params.name];
+    var description = blocks[request.blockName];
     if (!description) {
         response.status(404).json('No descriptiobn found for ' + request.params.name);
     } else{
     
     response.json(description);
+    }
+});
+
+app.get('/locations/:name', function(request, response){
+    var location = locations[request.blockName];
+        if(!location) {
+        response.status(404).json("No location found for " + request.params.name);
+    } else {
+    response.json(location);
+    }
+});
+
+app.get('/blocks', function(request, response){
+    if (request.query.limit >= 0){
+        response.json(blocks.slice(0, request.query.limit)); 
+    } else {
+    response.json(Object.keys(blocks));
     }
 });
         
